719444d7000206ad0870c108a13edfc870c74b26
jeongle <jyjjeong@gmail.com>
2018-12-04 23:25:46 -0800

    woop

--- client/components/TodoForm.jsx
+++ client/components/TodoForm.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../styles/form.css';
 
 const TodoForm = (props) => {
   return(
     <form onSubmit={props.handleSubmit}>
-      <label>
-        ENTER TODO
+      <label className="label">
+        <div className="label">ENTER TODO</div>
         <input type="text" onChange={props.onChange}></input>
       </label>
       <input type="submit" value="submit"/>
